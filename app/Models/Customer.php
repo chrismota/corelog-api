dbb883e3740030d174cc6389ed74945aeba5aa97
Christian Mota <christiancorreiamota@gmail.com>
2026-09-22 11:25:15 -0300

    feat: add softDeletes for customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasUuids;
+    use HasUuids, SoftDeletes;
 
     protected $fillable = [
         'organization_id',
