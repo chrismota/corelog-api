864b27fe5fc023c475d16047ca7533c3bf2a7ad6
Christian Mota <christiancorreiamota@gmail.com>
2026-09-22 11:22:04 -0300

    feat: create customer routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -10,5 +11,11 @@ Route::prefix('v1')->group(function () {
         Route::get('auth/me', [AuthController::class, 'me']);
         Route::post('auth/refresh', [AuthController::class, 'refresh']);
         Route::post('auth/logout', [AuthController::class, 'logout']);
+
+        Route::get('customers', [CustomerController::class, 'index']);
+        Route::get('customers/{customerId}', [CustomerController::class, 'show']);
+        Route::post('customers', [CustomerController::class, 'store']);
+        Route::put('customers/{customerId}', [CustomerController::class, 'update']);
+        Route::delete('customers/{customerId}', [CustomerController::class, 'destroy']);
     });
 });
