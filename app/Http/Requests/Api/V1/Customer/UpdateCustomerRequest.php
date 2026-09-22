2668871f6f5a9646af5bea785320ccb129128ee4
Christian Mota <christiancorreiamota@gmail.com>
2026-09-22 11:21:18 -0300

    feat: create customer requests

---
+++ app/Http/Requests/Api/V1/Customer/UpdateCustomerRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:150'],
+            'document' => ['required', 'string', 'max:20'],
+            'email' => ['required', 'email'],
+            'phone' => ['required', 'string', 'max:20'],
+        ];
+    }
+}
